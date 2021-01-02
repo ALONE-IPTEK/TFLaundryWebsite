a0ccf0d2c5dab9cbb19f440d439a35e859df16d0
sityoy <tioirfanantoni@gmail.com>
2021-01-02 13:14:43 +0700

    Update

--- maps/index.php
+++ maps/index.php
@@ -1,5 +1,6 @@
 <div class="text-center">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.0415366907923!2d106.7144530145196!3d-6.125112795565027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a02b1684998cf%3A0x8437d10b565a0847!2sT%20%26%20F%20Laundry!5e0!3m2!1sid!2sid!4v1609179462232!5m2!1sid!2sid" width="250" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126936.97153029419!2d106.68721864745973!3d-6.160164032609576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7c9d110d719%3A0x300c5e82dd4b8a0!2sJakarta%20Barat%2C%20Kec.%20Kb.%20Jeruk%2C%20Kota%20Jakarta%20Barat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1609517567549!5m2!1sid!2sid" width="250" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 <br/>
-                
+              <a href="https://maps.google.com" >  Ke Maps Browser
             </div>
+            
