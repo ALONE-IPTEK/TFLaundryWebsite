d0b5a2bd0a3bba77d59a02326bd215095452a08b
sityoy <tioirfanantoni@gmail.com>
2022-06-06 15:49:33 +0700

    tambahan

--- admin/order/index.php
+++ admin/order/index.php
@@ -2,42 +2,49 @@
 <?php
 
 $aksi="order/proses.php";
+$timezone = "Asia/Jakarta";
+if(function_exists('date_default_timezone_set')) date_default_timezone_set($timezone);
+$date2=date('Y-m-d H:i:s');
                     $p=isset($_GET['aksi'])?$_GET['aksi']:null;
                     switch($p){
 default:
 echo "<div class='panel panel-border panel-primary'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Order</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Olah Karyawan</h3> 
                                     </div>  <div class='panel-body'> 
-                                   <hr>
+		 <hr>
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
-                                            <th><i class='icon-time'></i> Nama</th>
-                                            <th><i class='icon-time'></i> Jenis Laundry</th>
-                                            <th><i class='icon-signal'></i> Jenis Pembayaran/Kg</th>
-                                            <th><i class='icon-signal'></i> Tanggal Order/Kg</th>
-                                            <th><i class='icon-signal'></i> GPS</th>
-                                            <th><i class='icon-signal'></i> Status</th>
+										<th><i class='icon-terminal'></i> No.</th>
+									<th><i class='icon-terminal'></i> Nama</th>
+                                            <th><i class='icon-terminal'></i> Jenis Laundry</th>
+                                            <th><i class='icon-time'></i> Jenis Pembayaran</th>
+											<th><i class='icon-signal'></i> Tanggal Order</th>
+											<th><i class='icon-signal'></i> GPS</th>
+											<th><i class='icon-signal'></i> Status</th>
+
+											<th><i class='icon-signal'></i> Tanggal Ambil/Proses </th>
+
 										<th><i class='icon-chevron-right'></i> Aksi</th>
                                         </tr>
                                     </thead>
                                     <tbody>";
 							$i=1;
-							$tp=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM transaksi2 ORDER BY id");
+							$tp=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM transaksi2  ORDER BY id");
 							while($r=mysqli_fetch_array($tp)){
 						    //$hargaa = $r['harga'];
                              echo"<tr>
-									<td>$r[username]</td>
-
-                                    <td>$r[jenis_laundry]</td>
-									<td>$r[jenis_pembayaran]</td>
-									<td>$r[tgl_order]</td>
+							  <td>$i</td>
+							   <td>$r[username]</td>
+							    <td>$r[jenis_laundry]</td>
+								 <td>$r[jenis_pembayaran]</td>
+                                    <td>$r[tgl_order]</td>
 									<td>$r[gps]</td>
 									<td>$r[status]</td>
+									<td>$r[tgl_order2]</td>
 
-
-                                    <td><a class='btn btn-primary' href='?p=transaksi2&aksi=edit&id=$r[id]'><i class='icon-edit'></i>Edit</a>
+                                    <td><a class='btn btn-primary' href='?p=dataord&aksi=edit&id=$r[id]'><i class='icon-edit'></i>Edit</a>
 									 <a class='btn btn-danger' href='$aksi?act=hapus&id=$r[id]'><i class='icon-trash'></i>Hapus</td>
                                     
                                 </tr>";
@@ -54,24 +61,52 @@ break;
 case "edit":
 	$edit = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM transaksi2 WHERE id='$_GET[id]'");
     $r    = mysqli_fetch_array($edit);
-echo "<form method='post' action='transaksi2/proses.php?act=update' enctype='multipart/form-data'>";
+echo "<form method='post' action='order/proses.php?act=update' enctype='multipart/form-data'>";
 echo "<div class='panel panel-border panel-primary'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-list'></i> Edit Jenis Laundry</h3> 
+                 <h3 class='panel-title'><i class='fa fa-list'></i> Edit Karyawan</h3> 
                                     </div>  <div class='panel-body'> 
 					<input type='hidden' name='id' value='$r[id]'>
 					 <div class='control-group'>
 					   <div class='form-group'>
-                            <label>Jenis</label>
-                            <div class='span9'><input class='form-control' value='$r[jenis2]'  type='text' name='jenis2' /></div>
+                            <label>Nama</label>
+                            <div class='span9'><input class='form-control' value='$r[username]'  type='text' name='username' disabled/></div>
+                        </div>
+					   <div class='form-group'>
+                            <label>Jenis Laundry</label>
+                            <div class='span9'><input class='form-control' value='$r[jenis_laundry]'  type='text' name='jenis_laundry' /></div>
                         </div>						
 					   <div class='form-group'>
-                            <label>Harga/Kg</label>
-                            <div class='span9'><input class='form-control' size='16' type='number' value='$r[harga]' name='harga' /></div>
+                            <label>Jenis Pembayaran</label>
+                            <div class='span9'><input class='form-control' size='16' type='text' value='$r[jenis_pembayaran]' name='jenis_pembayaran' /></div>
                         </div>
+											   <div class='form-group'>
+                            <label>Tanggal Order</label>
+                            <div class='span9'><input class='form-control' size='16' type='year' value='$r[tgl_order]' name='tgl_order' /></div>
+                        </div>
+                                              
+											   <div class='form-group'>
+                            <label>GPS</label>
+                            <div class='span9'><input class='form-control' size='16' type='text' value='$r[gps]' name='telp' /></div>
+                        </div>
+											   <div class='form-group'>
+                            <label>Status</label>
+                            <div class='span9'>
+							<select class='form-control' name='status'>
+                            <option >$r[status]</option>
+							<option value='AMBIL/PROSES'>AMBIL/PROSES</option>
+							<option value='KIRIM'>KIRIM</option>
+                            
+							</select></div>
+                        </div>
+
+                        <div class='form-group'>
+                        <label>Tanggal Ambil/Proses</label>
+                        <div class='span9'><input class='form-control' size='16' type='year' value='$date2' name='tgl_order2' /></div>
+                    </div>
 					<Br>
 
-			<input type='submit' class='btn btn-primary' value='Update'> <a class='btn btn-danger' href='?p=harga'>Batal</a>
+			<input type='submit' class='btn btn-primary' value='Update'> <a class='btn btn-danger' href='?p=dataord'>Batal</a>
 		  </form>
 		</div> 
 		                  
@@ -81,8 +116,7 @@ echo "<div class='panel panel-border panel-primary'>
 		";
 echo "";
 break;
-
-			}//penutup switch
+			}
 ?>    
 </body>
 
--- admin/order/proses.php
+++ admin/order/proses.php
@@ -7,13 +7,12 @@
 	                            break;
                         case "hapus":
 mysqli_query($GLOBALS["___mysqli_ston"], "DELETE FROM transaksi2 WHERE id='$_GET[id]'");
-  header('location:../index.php?p=olahko');
+  header('location:../index.php?p=dataord');
 	                            break;
                         case "update":
-    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE transaksi2 SET jenis_laundry='$_POST[jenis_laundry]',jenis_pembayaran='$_POST[jenis_pembayaran]',tgl_order='$_POST[tgl_order]',  gps='$_POST[gps]', status='$_POST[status]', username='$_POST[username]' WHERE id='$_POST[id]'");
+    mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE transaksi2 SET jenis_laundry='$_POST[jenis_laundry]',jenis_pembayaran='$_POST[jenis_pembayaran]',tgl_order='$_POST[tgl_order]',gps='$_POST[gps]',status='$_POST[status]' WHERE id='$_POST[id]'");
 							 
-  header('location:../index.php?p=olahko');  
+  header('location:../index.php?p=dataord');  
 	}
                     ?>
-      
       
